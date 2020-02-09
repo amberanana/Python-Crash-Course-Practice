06280292d66e70051395b0585eb1ad27136b5da8
amberanana <32849773+amberanana@users.noreply.github.com>
2020-02-09 21:42:25 +0800

    Create 3-1_practice_lists.py

---
+++ 3-1_practice_lists.py
@@ -0,0 +1,16 @@
+#3-1 Names
+names = ["Madoka", "Homura", "QB"]
+print(names[0])
+print(names[1])
+print(names[2])
+
+#3-2 Greetings
+names = ["Madoka", "Homura", "QB"]
+print(names[0] + "ちゃん, " + "こんにちは" + "!")
+print(names[1] + "ちゃん, " + "こんにちは" + "!")
+print(names[2] + "ちゃん, " + "こんにちは" + "!")
+
+#3-3 List
+lists = ["car", "bike"]
+print("I would like to own a " + lists[0] + ".")
+print("I would like to own a " + lists[1] + ".")
