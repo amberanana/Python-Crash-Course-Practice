1d1f9c537af1e18e7ec979cae3665afe16a6d466
amberanana <32849773+amberanana@users.noreply.github.com>
2020-02-13 19:08:55 +0800

    Create 3-3_practice_sorting_list.py

---
+++ 3-3_practice_sorting_list.py
@@ -0,0 +1,31 @@
+# 3.3 Practice
+list = ["Kyoto", "Tokyo", "Seoul", "Busan", "Jeju"]
+print("Original order: ")
+print(list)
+
+print("\nAlphabetical: ")
+print(sorted(list))
+
+print("\nOriginal Order: ")
+print(list)
+
+print("\nReverse alphabetical: ")
+print(sorted(list, reverse=True))
+
+print("\nOriginal order: ")
+print(list)
+
+print("\nReversed: ")
+list.reverse()
+print(list)
+
+print("\nOriginal order: ")
+list.reverse()
+print(list)
+
+print("\nAlphabetical: ")
+list.sort()
+print(list)
+
+print("\nReverse alphabetical: ")
+list.sort(reverse=True)
