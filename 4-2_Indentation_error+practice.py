92dd3a328c1346f6b4980b85568827745625e13d
amberanana <32849773+amberanana@users.noreply.github.com>
2020-02-14 16:20:39 +0800

    Create 4-2_Indentation_error+practice.py

---
+++ 4-2_Indentation_error+practice.py
@@ -0,0 +1,22 @@
+#4.2 Avoid indentation error
+
+#4.2.1. Forget indentation
+magicians = ['alice', "david", 'carolina']
+for magician in magicians:
+ print(magician)
+
+#4.2.2 Forget to zoom the extra code
+
+#4.2.3 Unexpected indent
+message = "Hello Python world!"
+# print(message)
+# There is no need to indent print()
+
+#4.2.5 Forget colon
+
+# Practice
+list = ['Hawaiine', 'Margherita', 'Pepperoni']
+for pizza in list:
+ print(pizza)
+ print("I really " + pizza.title() + " pizza!" )
+print("I really love pizza.")
