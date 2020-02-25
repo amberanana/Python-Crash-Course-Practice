6ab0000816a6dbdd1d243541ccd6574928750ff6
amberanana <32849773+amberanana@users.noreply.github.com>
2020-02-25 21:45:52 +0800

    Create 4-4_list_slice.py

---
+++ 4-4_list_slice.py
@@ -0,0 +1,37 @@
+# 4.4 Using part of the list
+# 4.4.1 Slice
+players = ['charles', 'martina', 'michael', 'florence', 'eli']
+print(players[0:3])
+print(players[1:4])
+print(players[:4])
+print(players[2:])
+print(players[-3:])
+
+#4.4.2 遍历slice
+players = ['charles', 'martina', 'michael', 'florence', 'eli']
+print("Here are the first three players on my team:")
+for player in players[:3]:
+    print(player.title())
+
+#4.4.3
+my_foods = ['pizza', 'falafel', 'carrot cake']
+friend_foods = my_foods[:]
+
+print("My favorite foods are:")
+print(my_foods)
+
+print("\nMy friend's favorite foods are:")
+print(friend_foods)
+
+#To verify that we do have two lists
+my_foods = ['pizza', 'falafel', 'carrot cake']
+friend_foods = my_foods[:]
+
+my_foods.append('cannoli')
+friend_foods.append('ice cream')
+
+print("My favorite foods are:")
+print(my_foods)
+
+print("\nMy friend's favorite foods:")
+print(friend_foods)
