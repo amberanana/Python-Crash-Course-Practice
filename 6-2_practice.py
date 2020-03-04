f29331779224577c0f570ba9cd40e5e6125225a4
amberanana <32849773+amberanana@users.noreply.github.com>
2020-03-04 22:07:21 +0800

    Create 6-2_practice.py

---
+++ 6-2_practice.py
@@ -0,0 +1,36 @@
+# 6-2 Practice
+
+# 6-1
+person_0 = {'first_name': 'liam', 'last_name': 'l', 'age': '26', 'city': 'vancouver'}
+print(person_0['first_name'])
+print(person_0['last_name'])
+print(person_0['age'])
+print(person_0['city'])
+
+# 6-2
+favorite_number= {
+    'andrew': '3',
+    'sandy': '7',
+    'david': '4',
+    'eddie': '21',
+    'larry': '17',
+}
+print("Andrew's favorite number is " + favorite_number['andrew'] + ".")
+print("Sandy's favorite number is " + favorite_number['sandy'] + ".")
+print("David's favorite number is " + favorite_number['david'] + ".")
+print("Eddie's favorite number is " + favorite_number['eddie'] + ".")
+print("Larry's favorite number is " + favorite_number['larry'] + ".")
+
+# 6-3
+vocab_list = {
+    'tuple': '元祖',
+    'syntax': '语法',
+    'value': '值',
+    'path': '路径',
+    'index': '索引',
+}
+print("Tuple's means " + vocab_list['tuple'] + " in Chinese.")
+print("Syntax's means " + vocab_list['syntax'] + " in Chinese.")
+print("Value's means " + vocab_list['value'] + " in Chinese.")
+print("Path's means " + vocab_list['path'] + " in Chinese.")
+print("Index's means " + vocab_list['index'] + " in Chinese.")
