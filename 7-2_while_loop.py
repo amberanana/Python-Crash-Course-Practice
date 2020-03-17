c7ec0a361e4a31a18a5f8a39673d2ac6dc13c703
amberanana <32849773+amberanana@users.noreply.github.com>
2020-03-17 18:49:03 +0800

    Create 7-2_while_loop.py

---
+++ 7-2_while_loop.py
@@ -0,0 +1,57 @@
+# 7.2 While loop
+# 7.2.1 Using while loop
+# counting.py
+current_number = 1
+while current_number <= 5:
+    print(current_number)
+    current_number += 1
+
+# 7.2.2 Let the user to choose when to exit
+prompt = "\nTell me something, and I will repeat it back to you:"
+prompt += "\nEnter 'quit' to end the program."
+message = ""
+while message != 'quit':
+    message = input(prompt)
+
+    if message != 'quit':
+        print(message)
+
+# 7.2.3 Using signs
+prompt = "\nTell me something, and I will repeat it back to you:"
+prompt += "\nEnter 'quit' to end the program."
+
+active = True
+while active:
+    message = input(prompt)
+
+    if message == 'quit':
+        active = False
+    else:
+        print(message)
+
+# 7.2.4 Using break to quit the loop
+prompt = "\nPlease enter the name of a city you have visited:"
+prompt += "\n(Enter 'quit' when you are finished.)"
+
+while True:
+    city = input(prompt)
+
+    if city == 'quit':
+        break
+    else:
+        print("I'd love to go " + city.title() + "!")
+
+# 7.2.5 Using continue in the loop
+current_number = 0
+while current_number < 10:
+    current_number += 1
+    if current_number % 2 == 0:
+        continue
+
+    print(current_number)
+
+# 7.2.6 Avoiding infinite loop
+x = 1
+while x <= 5:
+    print(x)
+    x += 1
